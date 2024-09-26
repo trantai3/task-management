4f329e8ec441b73f13a396743302443e7332fee3
trantai3 <trantai09032003@gmail.com>
2024-09-26 23:30:20 +0700

    delete

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -101,6 +101,18 @@ module.exports.changeMulti = async (req, res) => {
                     message: "Cập nhật trạng thái thành công!"
                 })
                 break
+            case "delete":
+                await Task.updateMany({
+                    _id: { $in: ids }
+                }, {
+                    deleted: true,
+                    deletedAt: new Date()
+                })
+                res.json({
+                    code: 200,
+                    message: "Xóa thành công!"
+                })
+                break
             default:
                 res.json({
                     code: 400,
@@ -145,6 +157,28 @@ module.exports.edit = async (req, res) => {
         res.json({
             code: 200,
             message: "Cập nhật thành công!"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi!"
+        })
+    }
+}
+
+// [DELETE] /tasks/delete/:id
+module.exports.delete = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        await Task.updateOne({ _id: id }, {
+            deleted: true,
+            deletedAt: new Date()
+        })
+
+        res.json({
+            code: 200,
+            message: "Xóa thành công!"
         })
     } catch (error) {
         res.json({
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -16,4 +16,6 @@ router.post('/create', controller.create)
 
 router.patch('/edit/:id', controller.edit)
 
+router.delete('/delete/:id', controller.delete)
+
 module.exports = router
