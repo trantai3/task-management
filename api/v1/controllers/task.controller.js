9c559825e11c6054ab5dd53e1633628861d26f38
trantai3 <trantai09032003@gmail.com>
2024-09-26 22:25:47 +0700

    change-multi

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -66,7 +66,7 @@ module.exports.changeStatus = async (req, res) => {
         const id = req.params.id
 
         const status = req.body.status
-        
+
         await Task.updateOne({
             _id: id
         }, {
@@ -77,6 +77,37 @@ module.exports.changeStatus = async (req, res) => {
             code: 200,
             message: "Cập nhật trạng thái thành công!"
         })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Không tồn tại!"
+        })
+    }
+}
+
+// [PATCH] /tasks/change-multi
+module.exports.changeMulti = async (req, res) => {
+    try {
+        const { ids, key, value } = req.body
+        switch (key) {
+            case "status":
+                await Task.updateMany({
+                    _id: { $in: ids }
+                }, {
+                    status: value
+                })
+                res.json({
+                    code: 200,
+                    message: "Cập nhật trạng thái thành công!"
+                })
+                break
+            default:
+                res.json({
+                    code: 400,
+                    message: "Không tồn tại!"
+                })
+                break
+        }
     } catch (error) {
         res.json({
             code: 400,
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -10,4 +10,6 @@ router.get('/detail/:id', controller.detail)
 
 router.patch('/change-status/:id', controller.changeStatus)
 
+router.patch('/change-multi', controller.changeMulti)
+
 module.exports = router
