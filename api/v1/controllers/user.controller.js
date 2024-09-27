1cd5ce5907e0aedf151b68e78ae4ab53590bfec6
trantai3 <trantai09032003@gmail.com>
2024-09-27 21:12:21 +0700

    login

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.model')
 const md5 = require('md5')
-const { use } = require('../routes/task.route')
+
 // [POST] /users/register
 module.exports.register = async (req, res) => {
     req.body.password = md5(req.body.password)
@@ -34,4 +34,40 @@ module.exports.register = async (req, res) => {
         })
     }
 
+}
+
+// [POST] /users/login
+module.exports.login = async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if (!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại!"
+        }) 
+        return
+    }
+
+    if (md5(password) !== user.password) {
+        res.json({
+            code: 400,
+            message: "Sai mật khẩu!"
+        })
+        return
+    }
+
+    const token = user.token
+    res.cookie("token", token)
+
+    res.json({
+        code: 200,
+        message: "Đăng nhập thành công!",
+        token: token
+    })
 }
