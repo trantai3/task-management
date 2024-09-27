5dd96355ccb2c6c4e854478e88df27ffcf0cbcb3
trantai3 <trantai09032003@gmail.com>
2024-09-27 21:46:47 +0700

    send email to take otp

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,5 +1,9 @@
 const User = require('../models/user.model')
+const ForgotPassword = require('../models/forgot-password.model')
 const md5 = require('md5')
+
+const generateHelper = require('../../../helpers/generate')
+const sendMailHelper = require('../../../helpers/sendEmail')
 
 // [POST] /users/register
 module.exports.register = async (req, res) => {
@@ -69,5 +73,49 @@ module.exports.login = async (req, res) => {
         code: 200,
         message: "Đăng nhập thành công!",
         token: token
+    })
+}
+
+// [POST] /users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    const email = req.body.email
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if (!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại!"
+        })
+        return
+    } 
+
+    const otp = generateHelper.generateRandomNumber(8)
+    
+    const timeExpire = 5
+    // Lưu data vào database
+    const objectForgotPassword = {
+        email: email,
+        otp: otp,
+        expireAt: Date.now() + timeExpire*60
+    }
+
+    const forgotPassword = new ForgotPassword(objectForgotPassword)
+    await forgotPassword.save()
+
+    // Gửi OTP qua email user
+    const subject = "Mã OTP xác minh lấy lại mật khẩu"
+    const html = `
+      Mã OTP để lấy lại mật khẩu của bạn là <b>${otp}</b> (Sử dụng trong ${timeExpire} phút).
+      Vui lòng không chia sẻ mã OTP này với bất kì ai.
+    `
+    sendMailHelper.sendEmail(email, subject, html)
+
+    res.json({
+        code: 200,
+        message: "Đã gửi mã OTP qua email!"
     })
 }
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -7,4 +7,6 @@ router.post('/register', controller.register)
 
 router.post('/login', controller.login)
 
+router.post('/password/forgot', controller.forgotPassword)
+
 module.exports = router
