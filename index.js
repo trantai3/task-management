4c31f779440db1cb47f240d41313838ae0b7b5ec
trantai3 <trantai09032003@gmail.com>
2024-09-27 16:08:19 +0700

    CORS

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 require("dotenv").config()
 const database = require('./config/database')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const routesAPI = require('./api/v1/routes/index.route')
-
 
 database.connect()
 
 const app = express()
 const port = process.env.PORT
+
+// CORS
+app.use(cors())
 
 // parse application/json
 app.use(bodyParser.json())
