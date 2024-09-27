f8e29ae5363de15a2cc8d3b4bce5514ec07a209b
trantai3 <trantai09032003@gmail.com>
2024-09-27 23:24:20 +0700

    individual info

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -182,3 +182,20 @@ module.exports.resetPassword = async (req, res) => {
         message: "Đổi mật khẩu thành công!"
     })
 }
+
+// [GET] /users/detail
+module.exports.detail = async (req, res) => {
+    const token = req.cookies.token
+    
+    const user = await User.findOne({
+        token: token,
+        deleted: false
+    }).select("-password -token")
+
+
+    res.json({
+        code: 200,
+        message: "Thành công!",
+        info: user
+    })
+}
