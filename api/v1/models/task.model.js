dcae718e30b1edc98ededc8cb0691e617d6d6f9e
trantai3 <trantai09032003@gmail.com>
2024-09-29 21:37:30 +0700

    create child task (save id of parent task)

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -8,6 +8,7 @@ const taskSchema = new mongoose.Schema(
     timeFinish: Date,
     createdBy: String,
     listUser: Array,
+    taskParentId: String,
     deleted: {
       type: Boolean,
       default: false,
